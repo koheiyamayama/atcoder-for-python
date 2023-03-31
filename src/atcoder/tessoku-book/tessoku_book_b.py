9fb430c2056fd8eae0126190e5079d37fe89f25f
koheiyamayama <kohei.y.23@gmail.com>
2023-04-01 01:37:49 +0900

    雛形作成

---
+++ src/atcoder/tessoku-book/tessoku_book_b.py
@@ -0,0 +1,12 @@
+# TODO edit this code, this code is for https://atcoder.jp/contests/practice/tasks/practice_1
+
+# param
+a = int(input())
+b, c = map(int, input().split())
+s = input()
+
+# solve
+ans = a + b + c
+
+# answer
+print("{} {}".format(ans, s))
